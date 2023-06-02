c858ac3f20ac486d3a68e642d2b8aa2d809a8160
zjw977020546 <11487928+zjw977020546@user.noreply.gitee.com>
2023-06-02 18:35:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+博客源码，基于 `VuePress Theme Hope` 搭建
+
+## 博客框架
+
+https://v2.vuepress.vuejs.org/zh/
+
+## 主题文档
+
+https://theme-hope.vuejs.press/zh/
+
+## 官方案例
+
+https://mrhope.site/
---
+++ src/.vuepress/config.ts
@@ -0,0 +1,38 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+import { searchPlugin } from "@vuepress/plugin-search";
+import { getDirname, path } from "@vuepress/utils";
+
+const __dirname = getDirname(import.meta.url);
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "XXXWeii",
+      description: "XXXWeii的博客",
+    },
+  },
+
+  theme,
+
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue"
+    ),
+
+  },
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+
+  plugins: [
+    searchPlugin({
+      // 你的选项
+    }),
+
+   ]
+});
