1c9c722cfe12735d7dc99ee23cd622ef93a3119e
zjw977020546 <11487928+zjw977020546@user.noreply.gitee.com>
2023-06-02 21:52:46 +0800

    first commit

--- README.md
+++ README.md
@@ -11,3 +11,24 @@ https://theme-hope.vuejs.press/zh/
 ## 官方案例
 
 https://mrhope.site/
+
+## 运行
+
+node 版本: v18 LTS
+
+```bash
+
+# 安装依赖
+npm install
+
+# 运行
+npm run docs:dev
+
+# 编译打包
+npm run docs:build
+
+
+# 发布部署到Github Page
+npm run deploy
+
+```
--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,10 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
+# Github仓库
 git push -f https://github.com/XXXWeii/XXXWeiiBlog.git main
+
+# Gitee仓库
+# git push -f https://gitee.com/XXXWeii/XXXWeiiBlog.git main
 
 cd -
